f667d071180cae2848b5ee1bbc5f9be2a90f9d7e
mataillade <matias.taillade@gmail.com>
2024-02-06 15:59:55 +0100

    add piece service

--- pychess/piece.py
+++ pychess/piece.py
@@ -15,29 +15,53 @@ class Piece(ABC):
 
 class King(Piece):
     def can_move(self, coordinates: Coordinates, target: Coordinates) -> bool:
-        pass
+        return (
+                abs(coordinates.x - target.x) <= 1
+                and abs(coordinates.y - target.y) <= 1
+                and coordinates != target
+        )
 
 
 class Queen(Piece):
     def can_move(self, coordinates: Coordinates, target: Coordinates) -> bool:
-        pass
+        return (
+                (coordinates.x == target.x
+                 or coordinates.y == target.y
+                 or abs(coordinates.x - target.x) == abs(coordinates.y - target.y))
+                and coordinates != target
+        )
 
 
 class Rooks(Piece):
     def can_move(self, coordinates: Coordinates, target: Coordinates) -> bool:
-        pass
+        return (
+                (coordinates.x == target.x or coordinates.y == target.y)
+                and coordinates != target
+        )
 
 
 class Bishop(Piece):
     def can_move(self, coordinates: Coordinates, target: Coordinates) -> bool:
-        pass
+        return (
+                abs(coordinates.x - target.x) == abs(coordinates.y - target.y)
+                and coordinates != target
+        )
 
 
 class Knight(Piece):
     def can_move(self, coordinates: Coordinates, target: Coordinates) -> bool:
-        pass
+        return (
+                ((abs(coordinates.x - target.x) == 2 and abs(coordinates.y - target.y) == 1)
+                 or (abs(coordinates.x - target.x) == 1 and abs(coordinates.y - target.y) == 2))
+                and coordinates != target
+        )
 
 
 class Pawn(Piece):
     def can_move(self, coordinates: Coordinates, target: Coordinates) -> bool:
-        pass
+        if coordinates != target:
+            if self.color == Colors.WHITE and coordinates.x == target.x and target.y - coordinates.y == 1:
+                return True
+            if self.color == Colors.BLACK and coordinates.x == target.x and target.y - coordinates.y == -1:
+                return True
+        return False
